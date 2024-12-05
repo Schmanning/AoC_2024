6634cf4fbd951ba6b804a4b8497275678884c80d
Jannik Witt <jannik.witt@muenchen.de>
2024-12-05 13:56:21 +0100

    Day1 changed

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -6,11 +6,7 @@ start = time.time()
 
 # Main
 l1, l2 = (np.sort(arr) for arr in np.array([[int(item) for item in row.split("   ")] for row in open("C:/dev/AdventOfCode/2024/Day1/input.txt", "r").read().split("\n")]).T)
-result1 = sum(abs(l1[x] - l2[x]) for x in range(len(l1)))
-result2 = sum(l1[x] * np.count_nonzero(l2 == l1[x]) for x in range(len(l1)))
-
-print("Part 1:\t", result1)
-print("Part 2:\t", result2)
+print("Part 1:\t", sum(abs(l1[x] - l2[x]) for x in range(len(l1))), "\nPart 2:\t", sum(l1[x] * np.count_nonzero(l2 == l1[x]) for x in range(len(l1))))
 
 ende = time.time()
 print('Zeit:   {:.3f}s'.format(ende-start))
